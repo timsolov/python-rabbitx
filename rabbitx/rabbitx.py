e473a66acba82fdf514a257680c346b11010722f
Timofey Solovyev <timsolodev@gmail.com>
2025-06-16 20:03:05 +0200

    improve authentication and network validation in RabbitX class

--- rabbitx/rabbitx.py
+++ rabbitx/rabbitx.py
@@ -32,9 +32,9 @@ class RabbitX:
     def __init__(
         self,
         network: str,
-        wallet: Wallet = None,
-        api_key: ApiKey = None,
-        base_url: str = None,
+        wallet: Wallet | None = None,
+        api_key: ApiKey | None = None,
+        base_url: str | None = None,
     ):
         """
         :param network: The network to use (supported: "ethereum", "ethereum-sepolia", "blast", "blast-sepolia")
@@ -47,12 +47,8 @@ class RabbitX:
         :type base_url: str
         """
 
-        if wallet is not None and api_key is not None:
-            raise Exception("Cannot provide both wallet and api_key")
-        if wallet is None and api_key is None:
-            raise Exception("Must provide either wallet or api_key")
-        if network not in API_URL:
-            raise ValueError(f"Invalid network: {network}")
+        self._validate_auth(wallet, api_key)
+        self._validate_network(network)
 
         self.network = network
 
@@ -77,3 +73,28 @@ class RabbitX:
         self.orders = Orders(self.transport)
         self.markets = Markets(self.transport)
         self.vaults = Vaults(self.transport)
+
+    def _validate_auth(self, wallet: Wallet, api_key: ApiKey):
+        """
+        Raises an Exception if both wallet and api_key are provided.
+        Raises an Exception if neither wallet nor api_key are provided.
+        """
+        auth_provided = 0
+        if wallet:
+            auth_provided += 1
+        if api_key:
+            auth_provided += 1
+
+        if auth_provided > 1:
+            raise Exception("Cannot provide both wallet and api_key")
+        if auth_provided == 0:
+            raise Exception("Must provide either wallet or api_key")
+
+    def _validate_network(self, network: str):
+        """
+        Raises an Exception if the network is invalid.
+        """
+        if network not in API_URL:
+            raise ValueError(f"Unknown network: {network}")
+
+
